95fd139f42e20fcdd57c1a2375c311b4327141c2
Tomrel <Aaron@Tomrel.com>
2017-01-21 01:56:02 -0700

    Initial Player checkin
    for initial Player

---
+++ EchoingDarkness/Assets/Code/LightingRender.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class LightingRender : MonoBehaviour {
+    protected int width;
+    protected int height;
+    protected RenderTexture target;
+    protected Camera camComponent;
+    public ScreenOverlay overlay;
+    // Use this for initialization
+    void Start () {
+        camComponent = GetComponent<Camera>();
+        target = new RenderTexture(Screen.width, Screen.height, 0);
+        width = Screen.width;
+        height = Screen.height;
+        target.name = "TextureFromCamera_" + gameObject.name;
+        target.Create();
+        camComponent.targetTexture = target;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (width != Screen.width || height != Screen.height)
+        {
+            target.Release();
+            Object.Destroy(target);
+            target = new RenderTexture(Screen.width, Screen.height, 0);
+            width = Screen.width;
+            height = Screen.height;
+            target.name = "TextureFromCamera_" + gameObject.name;
+            target.Create();
+            camComponent.targetTexture = target;
+        }
+
+        if (overlay != null)
+        {
+            overlay.texture = target;
+        }
+        
+    }
+}
---
+++ EchoingDarkness/Assets/Code/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    private Rigidbody2D rb;
+    public GameObject EchoLight;
+    public float EchoSpeed = 20f;
+    public float EchoRetract = 5f;
+    public float MaxEcho = 20f;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
+
+        rb.AddForce(movement * speed * Time.deltaTime);
+
+        if(EchoLight != null)
+        {
+            if(Input.GetButton("Echo"))
+            {
+                if (EchoLight.transform.localScale.x < MaxEcho)
+                {
+                    EchoLight.transform.localScale += new Vector3(EchoSpeed * Time.deltaTime, EchoSpeed * Time.deltaTime, EchoSpeed * Time.deltaTime);
+                }
+
+            }
+            else if(EchoLight.transform.localScale.x > 1)
+            {
+                EchoLight.transform.localScale -= new Vector3(EchoRetract * Time.deltaTime, EchoRetract * Time.deltaTime, EchoRetract * Time.deltaTime);
+            }
+        }
+    }
+}

ecd24f1b437ef3deb7e193163ade0cd34d30520a
Tomrel <Aaron@Tomrel.com>
2017-01-22 11:06:16 -0700

    SwitchRoom

---
+++ EchoingDarkness/Assets/Code/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    public SwitchControls[] Switches;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool isSwitchesPressed = true;
+        foreach (SwitchControls s in Switches)
+        {
+            if(!s.isSwitchActive)
+            {
+                isSwitchesPressed = false;
+                break;
+            }
+        }
+
+        if(isSwitchesPressed)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ EchoingDarkness/Assets/Code/SwitchControls.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchControls : MonoBehaviour {
+
+    public float SwitchActivationTime = 2f;
+    public Color SwitchInactiveColor = Color.cyan;
+    public Color SwitchActiveColor = Color.magenta;
+    protected float TimeActive = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public bool isSwitchActive
+    {
+        get
+        {
+            return TimeActive > 0f;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+        if (TimeActive > 0f)
+        {
+            sr.color = SwitchActiveColor;
+            TimeActive -= Time.deltaTime;
+        }
+        else
+        {
+            sr.color = SwitchInactiveColor;
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "PlayerLight")
+        {
+            TimeActive = SwitchActivationTime;
+        }
+
+    }
+}

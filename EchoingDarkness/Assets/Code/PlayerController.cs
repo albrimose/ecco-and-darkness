2a5d1b95073aaf72eb3dd8de86ea1229db2f1e8e
Meeinotl <meeinotl@yahoo.com>
2017-01-21 20:40:16 -0700

    Added Sound to Swimming

--- EchoingDarkness/Assets/Code/PlayerController.cs
+++ EchoingDarkness/Assets/Code/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb;
     public GameObject EchoLight;
+	public AudioSource audio;
+	public AudioClip EchoSound;
+	public AudioClip[] SwimSounds;
+	public AudioClip[] FastSwimSounds;
     public float EchoSpeed = 20f;
     public float DashSpeed = 600f;
     public float DashCooldown = 2f;
@@ -53,15 +57,28 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
-        if (Input.GetButton("Dash") && DashTimeTil <= 0f)
-        {
-            DashTimeTil = DashCooldown;
-            rb.AddForce(movement * DashSpeed * Time.deltaTime, ForceMode2D.Impulse);
-        }
+		if (Input.GetButton ("Dash") && DashTimeTil <= 0f) {
+			DashTimeTil = DashCooldown;
+			rb.AddForce (movement * DashSpeed * Time.deltaTime, ForceMode2D.Impulse);
+
+			if (!audio.isPlaying) 
+			{
+				audio.clip = FastSwimSounds [Random.Range (0, FastSwimSounds.Length)];
+				audio.Play ();
+			}
+		} 
+		else if (moveHorizontal != 0f || moveVertical != 0f)
+		{
+			if (!audio.isPlaying) 
+			{
+				audio.clip = SwimSounds [Random.Range (0, SwimSounds.Length)];
+				audio.Play ();
+			}
+		}
         
         if(DashTimeTil > 0f)
         {
-            Debug.Log(DashTimeTil);
+            //Debug.Log(DashTimeTil);
             DashTimeTil -= Time.deltaTime;
         }
         rb.AddForce(movement * speed * Time.deltaTime);
@@ -69,14 +86,14 @@ public class PlayerController : MonoBehaviour {
 
         if(EchoLight != null)
         {
-			AudioSource audio = GetComponent<AudioSource>();
             if(Input.GetButtonDown("Echo"))
             {
+				audio.clip = EchoSound;
 				audio.Play();
             }
 			if(Input.GetButtonUp("Echo"))
 			{
-				audio.Stop();
+				//audio.Stop();
 			}
             if(Input.GetButton("Echo"))
             {
